b7a101cae0afcebc0ee0a061e6d49919bba8ce6a
sergio-ridaura <sergio.ridaura@outlook.com>
2024-05-21 23:35:20 +0200

    Tailwind CSS basics

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,38 @@
+import colors from "tailwindcss/colors";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Atkinson"],
+      },
+      colors: {
+        transparent: colors.transparent,
+        current: colors.current,
+        light: colors.white,
+        dark: colors.black,
+        black: colors.black,
+        white: colors.white,
+        gray: colors.gray,
+        primary: colors.indigo,
+        secondary: colors.teal,
+        success: colors.green,
+        danger: colors.red,
+        warning: colors.yellow,
+        info: colors.sky,
+        red: colors.red,
+        green: colors.green,
+        blue: colors.blue,
+        cyan: colors.cyan,
+        magenta: colors.pink,
+        yellow: colors.yellow,
+        orange: colors.orange,
+        pink: colors.pink,
+        purple: colors.purple,
+      },
+    },
   },
   plugins: [],
 };
