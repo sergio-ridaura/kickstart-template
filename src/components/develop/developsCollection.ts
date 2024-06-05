5d64d73d090f27ec9fcd4d75ab49f2e41ec55483
sergio-ridaura <sergio.ridaura@outlook.com>
2024-06-05 14:05:29 +0200

    update About

--- src/components/develop/developsCollection.ts
+++ src/components/develop/developsCollection.ts
@@ -3,24 +3,22 @@ import { PUBLIC_SITE_LANG, PUBLIC_SITE_LANG_DEFAULT } from "@src/consts";
 
 export const developsCollection = defineCollection({
   type: "content",
-  schema: z
-    .object({
-      isDraft: z.boolean().default(false),
-      language: z.enum(PUBLIC_SITE_LANG).default(PUBLIC_SITE_LANG_DEFAULT),
-      title: z.string(),
-      description: z.string(),
-      author: z.string(),
-      tags: z.array(z.string()).optional(),
-      pubDate: z.date(),
-      upDate: z.date().optional(),
-      image: z.string().optional(),
-      issue: z.object({
+  schema: z.object({
+    isDraft: z.boolean().default(false),
+    language: z.enum(PUBLIC_SITE_LANG).default(PUBLIC_SITE_LANG_DEFAULT),
+    title: z.string(),
+    description: z.string(),
+    author: z.string(),
+    tags: z.array(z.string()).optional(),
+    pubDate: z.date(),
+    upDate: z.date().optional(),
+    image: z.string().optional(),
+    issue: z
+      .object({
         title: z.string(),
         url: z.string(),
-      }).optional(),
-      efforts: z.number().optional(),
-    })
-    .refine((data) => ({
-      upDate: data.upDate ?? data.pubDate,
-    })),
+      })
+      .optional(),
+    efforts: z.number().optional(),
+  }),
 });
--- src/components/post/postsCollection.ts
+++ src/components/post/postsCollection.ts
@@ -3,19 +3,15 @@ import { PUBLIC_SITE_LANG, PUBLIC_SITE_LANG_DEFAULT } from "@src/consts";
 
 export const postsCollection = defineCollection({
   type: "content",
-  schema: z
-    .object({
-      isDraft: z.boolean().default(false),
-      language: z.enum(PUBLIC_SITE_LANG).default(PUBLIC_SITE_LANG_DEFAULT),
-      title: z.string(),
-      description: z.string(),
-      author: z.string(),
-      tags: z.array(z.string()).optional(),
-      pubDate: z.date(),
-      upDate: z.date().optional(),
-      image: z.string(),
-    })
-    .refine((data) => ({
-      upDate: data.upDate ?? data.pubDate,
-    })),
+  schema: z.object({
+    isDraft: z.boolean().default(false),
+    language: z.enum(PUBLIC_SITE_LANG).default(PUBLIC_SITE_LANG_DEFAULT),
+    title: z.string(),
+    description: z.string(),
+    author: z.string(),
+    tags: z.array(z.string()).optional(),
+    pubDate: z.date(),
+    upDate: z.date().optional(),
+    image: z.string(),
+  }),
 });
