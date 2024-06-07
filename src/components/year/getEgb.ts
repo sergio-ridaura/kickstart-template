936a63d7347ef9f9a5238bcf0e74e51a7d85a801
sergio-ridaura <sergio.ridaura@outlook.com>
2024-06-07 09:57:17 +0200

    update About

--- src/components/year/getEgb.ts
+++ src/components/year/getEgb.ts
@@ -1,12 +1,15 @@
-import { getYear } from "./get";
+import { getEgbAge, type GetEgbAgeType } from "@cmp/age";
 
 /**
  * Calculates the year a person started grade EGB (Educación General Básica) based on birth year and grade.
  */
 export const getEgbYear = (
   birthYear: number,
-  grade: number = 1,
-  current: number = getYear()
+  grade: number | undefined = 1
 ): number => {
-  return birthYear + grade + current;
+  const validGrade: GetEgbAgeType =
+    grade >= 1 && grade <= 8 ? (grade as GetEgbAgeType) : 1;
+  const startAge = getEgbAge(validGrade);
+
+  return birthYear + startAge;
 };
