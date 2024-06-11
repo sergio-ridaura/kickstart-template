362c3e2b860021b85100b0481f693895981c2ae0
sergio-ridaura <sergio.ridaura@outlook.com>
2024-06-11 20:03:49 +0200

    node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
+import "dotenv/config";
 import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 
-// https://astro.build/config
-export default defineConfig({});
+const conf = {};
+
+if (process.env.PUBLIC_SITE_ADAPTER === "node") {
+  conf.output = "server";
+  conf.adapter = node({
+    mode: "standalone",
+  });
+}
+
+export default defineConfig({
+  ...conf,
+});
