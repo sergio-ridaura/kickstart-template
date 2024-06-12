f16ef17812a01599fabae10a987d0c97417787fd
sergio-ridaura <sergio.ridaura@outlook.com>
2024-06-12 10:29:32 +0200

    exports

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
-import defaultHello from "./components/ts/hello/default.js";
+// import defaultHello from "./components/ts/hello/default.js";
+import { defaultHello } from "kickstart-temp";
 
 console.log(defaultHello("in CLI"));
--- src/exports.ts
+++ src/exports.ts
@@ -1,12 +1,12 @@
-import isAdultAge from "./components/ts/age/isAdult";
-import randomArray from "./components/ts/array/random";
-import getFormattedDate from "./components/ts/date/getFormatted";
-import { getAgeEgb, type GetAgeEgbType } from "./components/ts/egb/getAge";
-import getYearEgb from "./components/ts/egb/getYear";
-import defaultHello from "./components/ts/hello/default";
-import capitalizeFirstString from "./components/ts/string/capitalize";
-import getYear from "./components/ts/year/get";
-import pastYears from "./components/ts/year/past";
+import isAdultAge from "./components/ts/age/isAdult.js";
+import randomArray from "./components/ts/array/random.js";
+import getFormattedDate from "./components/ts/date/getFormatted.js";
+import { getAgeEgb, type GetAgeEgbType } from "./components/ts/egb/getAge.js";
+import getYearEgb from "./components/ts/egb/getYear.js";
+import defaultHello from "./components/ts/hello/default.js";
+import capitalizeFirstString from "./components/ts/string/capitalize.js";
+import getYear from "./components/ts/year/get.js";
+import pastYears from "./components/ts/year/past.js";
 
 export {
   isAdultAge,
